5ba89553fe21f50a753439cc434f34a35368baa0
Gabriel Stedile <gabriel.stedile9@gmail.com>
2018-05-17 17:48:31 -0300

    Fix code with standardJs

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,36 @@
-var path = require('path');
-var nsp = require('gulp-nsp');
-var gulp = require('gulp');
-var excludeGitignore = require('gulp-exclude-gitignore');
-var eslint = require('gulp-eslint');
-var del = require('del');
-var babel = require('gulp-babel');
+var path = require('path')
+var nsp = require('gulp-nsp')
+var gulp = require('gulp')
+var excludeGitignore = require('gulp-exclude-gitignore')
+var eslint = require('gulp-eslint')
+var del = require('del')
+var babel = require('gulp-babel')
 
 // Initialize the babel transpiler so ES2015 files gets compiled
 // when they're loaded
-require('babel-core/register');
+require('babel-core/register')
 
 gulp.task('static', function () {
   return gulp.src('lib/**/*.js')
     .pipe(excludeGitignore())
     .pipe(eslint())
     .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
-});
+    .pipe(eslint.failAfterError())
+})
 
 gulp.task('nsp', function (cb) {
-  nsp({ package: path.resolve('package.json') }, cb);
-});
+  nsp({ package: path.resolve('package.json') }, cb)
+})
 
 gulp.task('clean', function () {
-  return del('dist');
-});
+  return del('dist')
+})
 
 gulp.task('babel', ['clean'], function () {
   return gulp.src('lib/**/*.js')
     .pipe(babel())
-    .pipe(gulp.dest('dist'));
-});
+    .pipe(gulp.dest('dist'))
+})
 
-gulp.task('prepublish', ['nsp', 'babel']);
-gulp.task('default', ['static']);
+gulp.task('prepublish', ['nsp', 'babel'])
+gulp.task('default', ['static'])
